cd36c07e8d83d652f9aa57644e886345653fe7bf
ranjan7707 <ranjan7707@gmail.com>
2023-07-09 19:44:31 +0530

    ExpressJs Q5

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+//shop.js
+const express = require('express');
+
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+
+router.get('/',(req,res,next) => {
+    res.sendFile(path.join(rootDir, 'views', '/shop.html'));
+});
+
+
+module.exports = router;
---
+++ util/path.js
@@ -0,0 +1,5 @@
+//path.js
+const path = require('path');
+
+module.exports = path.dirname(process.mainModule.filename);
+
